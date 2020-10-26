cf3aa597930c8255aed8cbd5648aafa567317f11
NikoReva <57528503+NikoReva@users.noreply.github.com>
2020-10-26 23:42:15 +0100

    bugfixing

--- src/Var.java
+++ src/Var.java
@@ -52,7 +52,7 @@ public class Var {
 	static int timelapsems = 999;
 	static int activetrack = 1;
 	static long highscore = 0;
-	static int coinstovictory = 30;
+	static int coinstovictory = 600;
 	
 	// Flags (booleans)
 	static boolean gamerunning = false, instartscreen = true, inmenue = false, inoptions = false, inshop = false;
